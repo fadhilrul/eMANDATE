27b115111ad48b2f3b56a55db998b7bb68e33cb9
Fadhilrul <fadfhz@gmail.com>
2020-09-24 17:14:31 +0800

    add oracle plugin

--- public/index.php
+++ public/index.php
@@ -987,7 +987,7 @@
                     <tr class="text-gray-700 dark:text-gray-400">
                       <td class="px-4 py-3">
                         <div class="flex items-center text-sm">
-                          <!-- Avatar with inset shadow -->
+                          Avatar with inset shadow
                           <div
                             class="relative hidden w-8 h-8 mr-3 rounded-full md:block"
                           >
