a3ebf2b27afe433e81562b01f7a78518fb5ccbad
Fadhilrul <fadfhz@gmail.com>
2020-10-01 15:45:53 +0800

    designtable

--- resources/views/pages/ENRPFileListDetails.blade.php
+++ resources/views/pages/ENRPFileListDetails.blade.php
@@ -87,9 +87,9 @@
       </tbody>
   </table> --}}
 
-  <h4 class="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">
+  {{-- <h4 class="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">
     <br>
-      Senarai ENRP E-Mandate
+        ENRP E-Mandate Details
    </h4>
   <div class="w-full overflow-hidden rounded-lg shadow-xs">
    <div class="w-full overflow-x-auto">
@@ -265,7 +265,140 @@
     </table>
   </div>
   {{ $NERPS_details->links() }}
-</div>
+</div> --}}
+
+
+
+
+<main class="h-full pb-16 overflow-y-auto">
+  <div class=" grid px-6 mx-auto">
+    <h2
+      class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200"
+    >
+      Senarai Fail E-Mandate
+    </h2>
+    <!-- CTA -->
+    <a
+      class="flex items-center justify-between p-4 mb-8 text-sm font-semibold text-purple-100 bg-purple-600 rounded-lg shadow-md focus:outline-none focus:shadow-outline-purple"
+      href="https://github.com/estevanmaito/windmill-dashboard"
+    >
+      <div class="flex items-center">
+        <svg
+          class="w-5 h-5 mr-2"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+        >
+          <path
+            d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+          ></path>
+        </svg>
+        <span>Star this project on GitHub</span>
+      </div>
+      <span>View more &RightArrow;</span>
+    </a>
+
+    <!-- With avatar -->
+    <h4
+      class="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300"
+    >
+    </h4>
+    <div class="w-full mb-8 overflow-hidden rounded-lg shadow-xs">
+      <div class="w-full overflow-x-auto">
+        <table class="w-full whitespace-no-wrap table-auto">
+          <thead>
+            <tr
+              class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
+              <th colspan="2" class="px-4 py-3"><b>Makluamat Ringkasan ENRP</b></th>
+            </tr>
+          </thead>
+          <tbody
+            class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800">
+            @foreach ($NERPS_details as $item)
+
+            <tr
+              class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
+              <td class="px-4 py-3">Nama</td><td class="px-4 py-3">{{ $item->filename }}</td>
+            </tr>
+
+            <tr
+              class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
+              <td class="px-4 py-3">No Akaun</td> <td class="px-4 py-3">{{ $item->payrefnum }}</td>
+            </tr>
+
+            <tr
+              class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
+              <td class="px-4 py-3">Bahagian</td> <td class="px-4 py-3">{{ $item->section }}</td>
+            </tr>
+
+            <tr
+              class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
+              <td class="px-4 py-3">IC No</td> <td class="px-4 py-3">{{ $item->idnum }}</td>
+            </tr>
+
+            <tr
+              class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
+              <td class="px-4 py-3">Nama Pembeli</td> <td class="px-4 py-3"> {{ $item->buyername }}</td>
+            </tr>
+
+            <tr
+              class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
+              <td class="px-4 py-3">Jumlah Debit</td> <td class="px-4 py-3">{{ $item->debitamt }}</td>
+            </tr>
+
+            <tr
+              class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
+              <td class="px-4 py-3">Tujuan</td> <td class="px-4 py-3">{{ $item->purpose }}</td>
+            </tr>
+
+            <tr
+              class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
+              <td class="px-4 py-3">Tarikh Mula Potongan</td> <td class="px-4 py-3">{{ $item->effdate }}</td>
+            </tr>
+
+            <tr
+              class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
+              <td class="px-4 py-3">Tarikh Tamat</td> <td class="px-4 py-3">{{ $item->expdate }}</td>
+            </tr>
+
+            <tr
+              class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
+              <td class="px-4 py-3">Tarikh Memohon</td> <td class="px-4 py-3">{{ $item->appdate }}</td>
+            </tr>
+
+            <tr
+              class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
+              <td class="px-4 py-3">Tarikh Merch</td> <td class="px-4 py-3">{{ $item->merchdate }}</td>
+            </tr>
+
+            <tr
+              class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
+              <td class="px-4 py-3">Tarikh Lulus</td> <td class="px-4 py-3">{{ $item->appdate }}</td>
+            </tr>
+
+            <tr
+            class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
+            <td class="px-4 py-3">Status</td> <td class="px-4 py-3">{{ $item->approval }}</td>
+           </tr>
+            
+
+            @endforeach
+          </tbody>
+        </table>
+      </div>
+      <div
+        class="grid px-4 py-3 text-xs font-semibold tracking-wide text-gray-500 uppercase border-t dark:border-gray-700 bg-gray-50 sm:grid-cols-9 dark:text-gray-400 dark:bg-gray-800"
+      >
+        <span class="col-span-2"></span>
+        <!-- Pagination -->
+      </div>
+    </div>
+
+    <!-- With actions -->
+    
+  </div>
+</main>
 @endsection
+
+
 
 
