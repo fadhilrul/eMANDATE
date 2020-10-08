2e10cb5655025389c471923aba9d29e5ac6494a3
izzahrazali <nur.izzah@csc.net.my>
2020-10-08 23:47:22 +0800

    seacrhcft

---
+++ app/Http/Livewire/Search_cft.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\EMANDATE_CFT;
+use Livewire\Component;
+use Livewire\WithPagination;
+use App\User;
+
+
+class Search_cft extends Component
+{
+
+    use WithPagination;
+
+    public $searchCFTTerm = '';
+   
+
+    public function render()
+    {
+
+        $searchCFTTerm = '%'.$this->searchCFTTerm.'%';
+
+       // $a = EMANDATE_CFT::where('filename','like', $searchCFTTerm)->WHERE('status','99')->paginate(10);
+        //dd($a);
+        
+
+        /* use orwhere if want to search more than 1 field */
+        return view('livewire.searchcft',[
+            //'cftdata' => EMANDATE_CFT::where('filename','like', $searchCFTTerm)->WHERE('status','99')->paginate(10)
+            'cftdata' => EMANDATE_CFT::where('filename','like', $searchCFTTerm)->orwhere('buyeracc','like', $searchCFTTerm)->paginate(10)
+        ]);
+    }
+}
+
+
+
+ 
