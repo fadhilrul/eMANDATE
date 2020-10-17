4ea4d0fead7687464178eea89a010bca5c3e8bdc
izzahrazali <nur.izzah@csc.net.my>
2020-10-18 02:51:43 +0800

    updateinterfaceinfo

--- resources/views/pages/EmandateInfo.blade.php
+++ resources/views/pages/EmandateInfo.blade.php
@@ -43,9 +43,25 @@
 										<td class="px-4 py-4 border border-indigo-dark">{{ $item->telno }}</td>
 									</tr>
 									<tr>
+										<td class="px-4 py-4 border border-indigo-dark">Status</td>
+										<td class="px-4 py-4 border border-indigo-dark">{{ $item->status }}</td>
+									</tr>
+
+									<tr>
+										<td class="px-4 py-4 border border-indigo-dark">Jumlah Pinjaman</td>
+										<td class="px-4 py-4 border border-indigo-dark">{{ $item->approved_limit }}</td>
+									</tr>
+
+									<tr>
+										<td class="px-4 py-4 border border-indigo-dark">Jumlah Potongan Bulanan</td>
+										<td class="px-4 py-4 border border-indigo-dark">{{ $item->instal_amt }}</td>
+									</tr>
+
+									<tr>
 										<td class="px-4 py-4 border border-indigo-dark">Amount Debit</td>
 										<td class="px-4 py-4 border border-indigo-dark">{{ $item->debitamt }}</td>
 									</tr>
+									
 									<tr>
 										<td class="px-4 py-4 border border-indigo-dark">Kekerapan</td>
 										<td class="px-4 py-4 border border-indigo-dark">{{ $item->freqnum }}</td>
@@ -82,29 +98,22 @@
 				<!-- Section for Maklumat Pembayaran -->
 				
 				<div class="bg-indigo-darker rounded shadow w-5/6 md:w-1/2 z-0 mx-auto -mt-1 md:-mt-0 md:-ml-1">
-					<div class="py-8 text-center text-indigo-lightest font-semibold uppercase">Maklumat Pembayaran</div>
+					<div class="py-8 text-center text-indigo-lightest font-semibold uppercase">Maklumat Potongan</div>
 					<hr class="mb-8 my-0 border border-indigo-dark opacity-25">
 					<div class="py-8">
 						<table class="w-3/4 mx-auto text-indigo-lightest text-center" style="border-collapse: collapse;">
 							<tbody>
+								
 								<tr>
-									<td class="px-4 py-4 border border-indigo-dark">Jumlah Pinjaman</td>
-									<td class="px-4 py-4 border border-indigo-dark">{{ $item->approved_limit }}</td>
-								</tr>
-								<tr>
-									<td class="px-4 py-4 border border-indigo-dark">Jumlah Bayaran Bulanan</td>
-									<td class="px-4 py-4 border border-indigo-dark">{{ $item->instal_amt }}</td>
-								</tr>
-								<tr>
-									<td class="px-4 py-4 border border-indigo-dark">Tarikh Terakhir Pusingan Bayaran</td>
+									<td class="px-4 py-4 border border-indigo-dark">Tarikh Terakhir Arahan Potongan</td>
 									<td class="px-4 py-4 border border-indigo-dark">{{ $item->lastcycle_date }}</td>
 								</tr>
 								<tr>
-									<td class="px-4 py-4 border border-indigo-dark">Tarikh Seterusnya Pusingan Bayaran</td>
+									<td class="px-4 py-4 border border-indigo-dark">Tarikh Berikutnya Arahan Potongan</td>
 									<td class="px-4 py-4 border border-indigo-dark">{{ $item->nextcycle_date }}</td>
 								</tr>
 								<tr>
-									<td class="px-4 py-4 border border-indigo-dark">Jumlah Gagal Pembayaran</td>
+									<td class="px-4 py-4 border border-indigo-dark">Jumlah Gagal Potongan Bulanan</td>
 									<td class="px-4 py-4 border border-indigo-dark">{{ $item->blockpayment_flag }}</td>
 								</tr>
 								
@@ -114,6 +123,43 @@
 					
 				</div>
 			</div>
+
+			<div class="w-full p-3">
+                    <!--Table Card-->
+                    <div class="bg-gray border border-grey-darkest rounded shadow">
+
+                        <div class="border-b border-grey-darkest p-3">
+                            <h5 class="uppercase text-grey-dark">Rekod Pendaftaran</h5>
+                        </div>
+                        <div class="p-5">
+                            <table class="w-full p-5 text-grey-darker">
+                                <thead>
+                                    <tr>
+                                        <th class="text-left text-grey-dark">Nama Fail</th>
+                                        <th class="text-left text-grey-dark">Tarikh</th>
+                                        <th class="text-left text-grey-dark">Status</th>
+                                    </tr>
+                                </thead>
+								
+                                <tbody>
+                                    <tr>
+                                        <td>{{ $item->filename }}</td>
+                                    
+                                        <td>{{ $item->effdate }}</td>
+                                    
+                                        <td>{{ $item->status }}</td>
+                                    </tr>                                   
+                                </tbody>
+                            </table>
+
+
+                        </div>
+                    </div>
+                    <!--/table Card-->
+            </div>
+
+		<br>
+
 		</div>
 	</div>
 	
@@ -127,6 +173,7 @@
 			</div>
 	</a>
 
+<br>
 <br>
 
 </section>
