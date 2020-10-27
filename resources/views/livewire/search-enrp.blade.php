1f3ed5534d601b07f5083bf8b663bfca3ebb96c8
izzahrazali <nur.izzah@csc.net.my>
2020-10-27 11:20:35 +0800

    search_main

--- resources/views/livewire/search-enrp.blade.php
+++ resources/views/livewire/search-enrp.blade.php
@@ -1,24 +1,12 @@
-@extends('pages.layout.app')
-@section('content')
-
-<style>
-  @media (min-width: 1280px){
-    .container {
-    max-width: 100vw !important;
-   }
-  }
-</style>
-
-<div class="container px-6 mx-auto grid">
-     <div class="main-content flex-1 bg-gray-100 mt-12 md:mt-2 pb-24 md:pb-5">
-
-            <div class="bg-blue-800 p-2 shadow text-xl text-white">
-                <h3 class="font-bold pl-2">Senarai Nama Fail E-Mandate</h3>
-            </div>
-
-            <!--  START TABLES  -->
-            <div class="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 pr-10 lg:px-8">
-              <div class="align-middle rounded-tl-lg rounded-tr-lg inline-block w-full py-4 overflow-hidden bg-white shadow-lg px-12">
+<div class="max-width: 100vw px-6 mx-auto grid">
+	<div class="main-content flex-1 bg-gray-100 mt-12 md:mt-2 pb-24 md:pb-5">
+		<div class="bg-blue-800 p-2 shadow text-xl text-white">
+            <h3 class="font-bold pl-2">Senarai Nama Fail E-Mandate</h3>
+        </div>
+		
+    <!-- search section -->
+		<div class="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 pr-10 lg:px-8">
+			  <div class="align-middle rounded-tl-lg rounded-tr-lg inline-block w-full py-4 overflow-hidden bg-white shadow-lg px-12">
                   <div class="flex justify-between">
                       <div class="inline-flex border rounded w-7/12 px-2 lg:px-6 h-12 bg-transparent">
                           <div class="flex flex-wrap items-stretch w-full h-full mb-6 relative">
@@ -30,54 +18,54 @@
                                       </svg>
                                   </span>
                               </div>
-                              <input type="text" class="flex-shrink flex-grow flex-auto leading-normal tracking-wide w-px flex-1 border border-none border-l-0 rounded rounded-l-none px-3 relative focus:outline-none text-xxs lg:text-xs lg:text-base text-gray-500 font-thin" placeholder="Search">
+                              <input type="text" class="flex-shrink flex-grow flex-auto leading-normal tracking-wide w-px flex-1 border border-none border-l-0 rounded rounded-l-none px-3 relative focus:outline-none text-xxs lg:text-xs lg:text-base text-gray-500 font-thin form-input" placeholder="Search"
+                              wire:model="searchlistenrp"
+                              class="form-control"/>
                           </div>
                       </div>
                   </div>
               </div>
-              <div class="align-middle inline-block min-w-full shadow overflow-hidden bg-white shadow-dashboard px-8 pt-3 rounded-bl-lg rounded-br-lg">
-                  <table class="min-w-full">
-                      <thead>
-                          <tr>
-                              <th class="px-6 py-3 border-b-2 border-gray-300 text-left leading-4 text-blue-500 tracking-wider">Nama Fail</th>
-                          </tr>
-                      </thead>
-                      @foreach ($file_ENRP as $item)
-                      <tbody class="bg-white">     
-                      <tr>
-                                  <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
-                                      <div class="flex items-center">
-                                          <div>
-                                              <div class="text-sm leading-5 text-gray-800">
+  
+			<div class="align-middle inline-block min-w-full shadow overflow-hidden bg-white shadow-dashboard px-8 pt-3 rounded-bl-lg rounded-br-lg">
+				<table class="min-w-full">
+				
+					<thead>
+                        <tr>
+                            <th class="px-6 py-3 border-b-2 border-gray-300 text-left leading-4 text-blue-500 tracking-wider">Nama Fail</th>
+                        </tr>
+                     </thead>
+				
+					 @foreach ($file_ENRP as $item)
+					<tbody class="bg-white">  
+						
+						<tr>
+							<td class="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
+                                <div class="flex items-center">
+                                    <div>
+                                        <div class="text-sm leading-5 text-gray-800">
 
-                                                 {{ $item->filename }}
+                                           {{ $item->filename }}
 
-                                              </div>
-                                          </div>
-                                      </div>
-                                  </td> 
-                                  <td class="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-500 text-sm leading-5">
+                                        </div>
+                                    </div>
+                                </div>
+                            </td>
+							 <td class="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-500 text-sm leading-5">
                                     <button class="px-5 py-2 border-blue-500 border text-blue-500 rounded transition duration-300 hover:bg-blue-700 hover:text-white focus:outline-none">
                                         <a href = "{{ url('linkmainenrp/'.$item->filename.'')}}"> Papar </a> </button>
                                 </td>
-                      </tr>
-                      @endforeach
-                      </tbody>
-                  </table>
-               <div class="sm:flex-1 sm:flex sm:items-center sm:justify-between mt-4 work-sans">
+						</tr>
+			 
+                    </tbody>
+					@endforeach
+			
+				</table>
+				<div class="sm:flex-1 sm:flex sm:items-center sm:justify-between mt-4 work-sans">
                </div>
-        <!--  END TABLES -->
-            </div>
-          </div>
-
-          </div>
-      </div>
-      {{-- <div>
-        <a href= '{{ url("emandate-dashboard") }}' >
-          <span class="ml-1 text-lg font-bold text-gray-800 dark:text-gray-200" style="color:purple"> HOME </span>
-        </a>
-      </div>  --}}
-  <!--****************************************************************************************************************************-->
-@endsection
+			</div>
+						
+		</div>
+	</div>
+</div>
 
 
