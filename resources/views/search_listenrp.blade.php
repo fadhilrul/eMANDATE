840eb96bfe62e4083bb07b6053931535f418174f
izzahrazali <nur.izzah@csc.net.my>
2020-10-27 11:34:15 +0800

    updateagain_searchmain

--- resources/views/search_listenrp.blade.php
+++ resources/views/search_listenrp.blade.php
@@ -1,16 +1,18 @@
 @extends('pages.layout.app')
 @section('content')
-<div class="container mt-4">
-    <div class="row">
-      <div class="col-md-8 offset-2">
-        <div class="card">
-          <div class="card-header bg-success text-white ">
-          </div>
-          <div class="card-body">
-            @livewire('search-enrp')
-          </div>
-        </div>
-      </div>
-    </div>
-</div>
+
+<style>
+  @media (min-width: 1280px){
+    .container {
+    max-width: 100vw !important;
+   }
+  }
+</style>
+
+
+  <div class="card-body">
+    @livewire('search-enrp')
+  </div>
+          
+
 @endsection
--- resources/views/searchboxcft.blade.php
+++ resources/views/searchboxcft.blade.php
@@ -1,6 +1,15 @@
 @extends('pages.layout.app')
 @section('content')
 
+<style>
+  @media (min-width: 1280px){
+    .container {
+    max-width: 100vw !important;
+   }
+  }
+</style>
+
+
           <div class="card-body">
             @livewire('search_cft')
           </div>
