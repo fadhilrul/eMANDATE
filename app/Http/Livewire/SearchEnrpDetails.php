310c242182b35c85fbfc01a08577dd203fff574b
izzahrazali <nur.izzah@csc.net.my>
2020-11-20 19:50:38 +0800

    updatev3

--- app/Http/Livewire/SearchEnrpDetails.php
+++ app/Http/Livewire/SearchEnrpDetails.php
@@ -5,6 +5,7 @@ namespace App\Http\Livewire;
 use App\Models\EMANDATE_ENRP;
 use Livewire\Component;
 use Livewire\WithPagination;
+use Illuminate\Support\Facades\DB;
 
 class SearchEnrpDetails extends Component
 {
@@ -22,15 +23,33 @@ class SearchEnrpDetails extends Component
     public function render()
     {
         $listenrp =  "%".$this->listenrp."%";
-         
+        $state_user = session('authenticatedUser')['state_code'];
+
         //$a = EMANDATE_ENRP::where('hcrdate','=', $this->idenrp)->get();
         //dd ($a);
 
-         return view('livewire.search-enrp-details',[
+        if ( $state_user == 00)
+        {    
+            return view('livewire.search-enrp-details',[
 
-            'filelist_ENRP' => EMANDATE_ENRP::where('hcrdate','=', $this->idenrp)//->paginate(10)
-                            ->where('payrefnum','like', $listenrp)
-                            ->paginate(10)
-        ]);
+                'filelist_ENRP' => EMANDATE_ENRP::where('hcrdate','=', $this->idenrp)//->paginate(10)
+                                ->where('payrefnum','like', $listenrp)
+                                ->paginate(10)
+            ]);
+        }
+        else{
+
+            return view('livewire.search-enrp-details',[
+
+                'filelist_ENRP' => EMANDATE_ENRP::where('hcrdate','=', $this->idenrp)//->paginate(10)
+                                    ->join ('ACCOUNT_MASTER', DB::raw("TRIM(ACCOUNT_MASTER.ACCOUNT_NO)"), '=', DB::raw("TRIM(EMANDATE_ENRP.PAYREFNUM)")  )
+                                    ->join ('BRANCHES', 'BRANCHES.BRANCH_CODE', '=', 'ACCOUNT_MASTER.BRANCH_CODE')
+                                    ->where('BRANCHES.STATE_CODE' , '=',  $state_user )
+                                    ->where('payrefnum','like', $listenrp)
+                                    ->paginate(10)
+            ]);
+        }
+
+
     }
 }
